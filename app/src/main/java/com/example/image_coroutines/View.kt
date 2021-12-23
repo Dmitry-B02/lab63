70662427ba15806bfb25dfefea5733777efc2e9b
Dmitry-B02 <silaxer@mail.ru>
2021-12-23 23:36:56 +0300

    1

--- app/src/main/java/com/example/image_coroutines/View.kt
+++ app/src/main/java/com/example/image_coroutines/View.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.net.URL
 
 class MainViewModel: ViewModel() {
@@ -16,9 +17,9 @@ class MainViewModel: ViewModel() {
         viewModelScope.launch(Dispatchers.IO) {
             val url = URL("https://i.ibb.co/LCGjry8/Screenshot-20211121-231052-Gallery.jpg")
             val stream = url.openConnection().getInputStream()
-            stream.use {
-                val bitmap = BitmapFactory.decodeStream(stream)
-                bitmapData.postValue(bitmap)
+            val bitmap = BitmapFactory.decodeStream(stream)
+            withContext(Dispatchers.Main) {
+                bitmapData.value = bitmap
             }
         }
     }
